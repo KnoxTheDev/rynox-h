498486d1adec477b3b54dc545a4907ff01c31331
Knoxius <tuxltuxl001@gmail.com>
2025-09-08 06:37:40 +0530

    Update MixinGameRenderer.java
    Signed-off-by: Knoxius <tuxltuxl001@gmail.com>

--- src/main/java/dev/knoxy/rynox/api/mixin/MixinGameRenderer.java
+++ src/main/java/dev/knoxy/rynox/api/mixin/MixinGameRenderer.java
@@ -1,54 +1,52 @@
 package dev.knoxy.rynox.api.mixin;
 
 import com.mojang.datafixers.util.Pair;
-import net.minecraft.client.render.shader.ShaderStage;
-import net.minecraft.util.hit.HitResult;
-import dev.knoxy.rynox.client.event.impl.TiltEvent;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-import dev.knoxy.rynox.client.shader.GlProgram;
+import dev.knoxy.rynox.client.Rynox;
 import dev.knoxy.rynox.client.event.impl.FloatingItemEvent;
 import dev.knoxy.rynox.client.event.impl.Render3DEvent;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.joml.Matrix4fc;
-import com.mojang.blaze3d.systems.RenderSystem;
+import dev.knoxy.rynox.client.event.impl.ReachEvent;
+import dev.knoxy.rynox.client.event.impl.TiltEvent;
+import dev.knoxy.rynox.client.shader.GlProgram;
 import dev.knoxy.rynox.client.util.impl.RenderHelper;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gl.ShaderProgram;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.client.util.math.MatrixStack;
-import org.spongepowered.asm.mixin.Overwrite;
-import net.minecraft.util.hit.EntityHitResult;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.decoration.ItemFrameEntity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.decoration.ItemFrameEntity;
+import net.minecraft.resource.ResourceFactory;
 import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.math.Position;
+import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-
-import java.util.function.Consumer;
-import java.util.function.Predicate;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.entity.projectile.ProjectileUtil;
-import dev.knoxy.rynox.client.event.impl.ReachEvent;
-import dev.knoxy.rynox.client.Rynox;
-import java.util.function.Function;
-import net.minecraft.client.gl.ShaderProgram;
-import net.minecraft.resource.ResourceFactory;
-import java.util.List;
 import org.spongepowered.asm.mixin.Final;
-import net.minecraft.client.MinecraftClient;
-import org.spongepowered.asm.mixin.Shadow;
-import net.minecraft.client.render.GameRenderer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture.CAPTURE_FAILHARD;
 
-@Mixin(value = { GameRenderer.class }, priority = 999)
-public class MixinGameRenderer
-{
+import java.util.List;
+import java.util.function.Consumer;
+
+@Mixin(value = GameRenderer.class, priority = 999)
+public class MixinGameRenderer {
+
     @Shadow
     public int floatingItemTimeLeft;
+
     @Shadow
     @Final
     public MinecraftClient client;
+
     public MinecraftClient mc;
 
     public MixinGameRenderer() {
@@ -67,37 +65,55 @@ public class MixinGameRenderer
         }
         client.getProfiler().push("pick");
         client.targetedEntity = null;
-        double n2 = client.interactionManager.getReachDistance();
+
+        double reach = client.interactionManager.getReachDistance();
         boolean invoke = false;
+
         if (!Rynox.Companion.getSelfDestructed()) {
             ReachEvent event = new ReachEvent(0);
             if (event.invoke()) {
-                n2 += event.getReach();
+                reach += event.getReach();
             }
         }
-        client.crosshairTarget = entity.raycast(n2, tickDelta, false);
+
+        client.crosshairTarget = entity.raycast(reach, tickDelta, false);
         Vec3d cameraPosVec = entity.getCameraPosVec(tickDelta);
-        boolean b = false;
-        double n3 = n2;
-        double n4;
+        boolean far = false;
+        double entityRange = reach;
+        double maxRange;
+
         if (!client.interactionManager.hasExtendedReach()) {
-            if (n3 > 3.0 && !invoke) {
-                b = true;
+            if (entityRange > 3.0 && !invoke) {
+                far = true;
             }
-            n4 = n3;
+            maxRange = entityRange;
         } else {
-            n3 = (n4 = 6.0);
+            entityRange = (maxRange = 6.0);
         }
-        double squaredDistanceTo = n3 * n3;
+
+        double squaredDistanceTo = entityRange * entityRange;
         if (client.crosshairTarget != null) {
             squaredDistanceTo = client.crosshairTarget.getPos().squaredDistanceTo(cameraPosVec);
         }
-        Vec3d vec = entity.getRotationVec(1);
-        EntityHitResult raycast = ProjectileUtil.raycast(entity, cameraPosVec, cameraPosVec.add(vec.x * n4, vec.y * n4, vec.z * n4), entity.getBoundingBox().stretch(vec.multiply(n4)).expand(1.0, 1.0, 1.0), e -> !e.isSpectator() && e.canHit(), squaredDistanceTo);
+
+        Vec3d vec = entity.getRotationVec(1.0F);
+        EntityHitResult raycast = ProjectileUtil.raycast(
+                entity,
+                cameraPosVec,
+                cameraPosVec.add(vec.x * maxRange, vec.y * maxRange, vec.z * maxRange),
+                entity.getBoundingBox().stretch(vec.multiply(maxRange)).expand(1.0, 1.0, 1.0),
+                e -> !e.isSpectator() && e.canHit(),
+                squaredDistanceTo
+        );
+
         if (raycast != null) {
             double d3 = cameraPosVec.squaredDistanceTo(raycast.getPos());
-            if (b && d3 > 9.0) {
-                client.crosshairTarget = BlockHitResult.createMissed(raycast.getPos(), Direction.getFacing(vec.x, vec.y, vec.z), BlockPos.ofFloored(raycast.getPos()));
+            if (far && d3 > 9.0) {
+                client.crosshairTarget = BlockHitResult.createMissed(
+                        raycast.getPos(),
+                        Direction.getFacing(vec.x, vec.y, vec.z),
+                        BlockPos.ofFloored(raycast.getPos())
+                );
             } else if (d3 < squaredDistanceTo || client.crosshairTarget == null) {
                 client.crosshairTarget = raycast;
                 if (raycast.getEntity() instanceof LivingEntity || raycast.getEntity() instanceof ItemFrameEntity) {
@@ -105,11 +121,24 @@ public class MixinGameRenderer
                 }
             }
         }
+
         client.getProfiler().pop();
     }
 
-    @Inject(at = { @At(value = "FIELD", target = "Lnet/minecraft/client/render/GameRenderer;renderHand:Z", opcode = 180, ordinal = 0) }, method = { "renderWorld" })
-    void render3dHook(float n, long n2, MatrixStack matrixStack, CallbackInfo callbackInfo) {
+    /**
+     * Capture projection/modelview/position matrices used inside GameRenderer.renderWorld.
+     * In 1.21.8 GameRenderer.renderWorld has the signature renderWorld(RenderTickCounter).
+     *
+     * We inject near the point where the renderer switches to drawing the hand (same conceptual spot
+     * as before) and capture the local MatrixStack produced by GameRenderer.renderWorld so RenderHelper
+     * can reuse the same matrices.
+     */
+    @Inject(
+            method = "renderWorld",
+            at = @At(value = "FIELD", target = "Lnet/minecraft/client/render/GameRenderer;renderHand:Z", opcode = 180, ordinal = 0),
+            locals = LocalCapture.CAPTURE_FAILHARD
+    )
+    void render3dHook(RenderTickCounter tickCounter, CallbackInfo ci, /* captured locals */ MatrixStack matrixStack) {
         if (!Rynox.Companion.getSelfDestructed()) {
             RenderHelper.getProjectionMatrix().set(RenderSystem.getProjectionMatrix());
             RenderHelper.getModelViewMatrix().set(RenderSystem.getModelViewMatrix());
@@ -117,16 +146,32 @@ public class MixinGameRenderer
         }
     }
 
-    @Inject(at = { @At(value = "INVOKE", target = "Lnet/minecraft/util/profiler/Profiler;swap(Ljava/lang/String;)V", ordinal = 1) }, method = { "renderWorld" }, cancellable = true)
-    void renderWorld(float n, long n2, MatrixStack matrixStack, CallbackInfo callbackInfo) {
+    /**
+     * Allow Render3DEvent to cancel world rendering.
+     * Inject near the profiler swap used inside renderWorld.
+     * renderWorld(RenderTickCounter) in 1.21.8: capture MatrixStack local to pass into the event.
+     */
+    @Inject(
+            method = "renderWorld",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiler/Profiler;swap(Ljava/lang/String;)V", ordinal = 1),
+            cancellable = true,
+            locals = LocalCapture.CAPTURE_FAILHARD
+    )
+    void renderWorld(RenderTickCounter tickCounter, CallbackInfo callbackInfo, MatrixStack matrixStack) {
         if (!Rynox.Companion.getSelfDestructed()) {
-            if (new Render3DEvent(matrixStack, n).invoke()) {
+            if (new Render3DEvent(matrixStack, tickCounter.getTickProgress(true)).invoke()) {
                 callbackInfo.cancel();
             }
         }
     }
 
-    @Inject(at = { @At(value = "FIELD", target = "Lnet/minecraft/client/render/GameRenderer;floatingItemTimeLeft:I", ordinal = 1) }, method = { "tick" })
+    /**
+     * Adjust floatingItemTimeLeft during tick.
+     */
+    @Inject(
+            method = "tick",
+            at = @At(value = "FIELD", target = "Lnet/minecraft/client/render/GameRenderer;floatingItemTimeLeft:I", ordinal = 1)
+    )
     void adjustFloatingTimeLeft(CallbackInfo callbackInfo) {
         if (!Rynox.Companion.getSelfDestructed()) {
             FloatingItemEvent event = new FloatingItemEvent(0);
@@ -135,13 +180,27 @@ public class MixinGameRenderer
         }
     }
 
-    @Inject(method = "loadPrograms", at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z", ordinal = 0), locals = LocalCapture.CAPTURE_FAILHARD)
-    void loadAllTheShaders(ResourceFactory factory, CallbackInfo ci, List<net.minecraft.client.render.shader.ShaderStage> stages, List<Pair<ShaderProgram, Consumer<ShaderProgram>>> shadersToLoad) {
+    /**
+     * In 1.21.8 the shader preload plumbing uses preloadPrograms(ResourceFactory).
+     * The local list of shader program pairs (shader program, consumer) is captured and we add our GlProgram loaders.
+     *
+     * This replaces the old loadPrograms/stages approach and uses the ShaderProgram type directly.
+     */
+    @Inject(
+            method = "preloadPrograms",
+            at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z", ordinal = 0),
+            locals = LocalCapture.CAPTURE_FAILHARD
+    )
+    void loadAllTheShaders(ResourceFactory factory, CallbackInfo ci, List<Pair<ShaderProgram, Consumer<ShaderProgram>>> shadersToLoad) {
+        // GlProgram.forEachProgram supplies loaders (left: factory -> ShaderProgram, right: consumer)
         GlProgram.forEachProgram(loader -> shadersToLoad.add(new Pair<>(loader.getLeft().apply(factory), loader.getRight())));
     }
 
-    @Inject(at = { @At("HEAD") }, method = { "tiltViewWhenHurt" }, cancellable = true)
-    void tiltViewWhenHurt(MatrixStack matrixStack, float n, CallbackInfo callbackInfo) {
+    /**
+     * Cancel tilt behavior when hurt if our event returns true.
+     */
+    @Inject(at = @At("HEAD"), method = "tiltViewWhenHurt", cancellable = true)
+    void tiltViewWhenHurt(MatrixStack matrixStack, float tickProgress, CallbackInfo callbackInfo) {
         if (!Rynox.Companion.getSelfDestructed()) {
             if (new TiltEvent().invoke()) {
                 callbackInfo.cancel();
